12cccc84f96b47c5bd83f5f838b61cef89ba0bd8
kavala <kvk12121999@g,ail.com>
2022-07-26 10:28:35 +0530

    "DB connected"

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb"
+  HOST: "us-cdbr-east-06.cleardb.net",
+  USER: "b2cb94ceefafac",
+  PASSWORD: "29af9580",
+  DB: "heroku_9da24a5ad488f24"
 };
